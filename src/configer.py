1cf2bb87091a79ebe4c2c6f1aadabcd9eab16891
LoengWaidak <liang.bade@gmail.com>
2024-01-21 11:16:31 +0800

    使用pytest，初步写好config的测试，configer增加检查报错功能

--- src/configer.py
+++ src/configer.py
@@ -1,14 +1,29 @@
 import configparser
 import threading
 import logging
+import os
 from utils import MAIN_LOG
 mainlog = logging.getLogger(MAIN_LOG)
 
 class Config:
     def __init__(self, filename='config.ini'):
         self.filename = filename
-        self.config = configparser.ConfigParser()
+        self.config = configparser.ConfigParser(interpolation=None)
+
+        try:
+            f = open(filename)
+            f.close()
+        except FileNotFoundError:
+            example = '''[LocalFiles]\n# 设备名称\ndevicename = 设备名称\n# 本地检测新文件目录\nlocaldirectory = 本地检测目录\n# 检测时间间隔 单位（分钟）\ncheckinterval = 30\n\n[BaiduCloud]\n# 本程序的百度应用\nappname = 摄影素材自动备份\nappid = 47097507\nappkey = H794OU88Q5KXH89ahoPGVCFNMxVBb1Sb\nsecretkey = pWjzs8MIBw2fxutAXsxVpN0Pxa0OqRT6\nsignkey = X3JHR8D=5g0!EP%RF1FzGDrMQFPQkn1V\n\n# 用户百度授权token，有的话可以输入，无可留空\naccesstoken = \nrefreshtoken = '''
+            with open(filename, "w") as f:
+                f.write(example)
+            mainlog.critical(f'配置文件不存在，已创建模版{filename}')
+            raise FileNotFoundError(f'配置文件不存在，已创建模版{filename}')
+
+        mainlog.debug(f'尝试读取配置{filename}')
         self.config.read(self.filename)
+        self._check_config_required()
+
         self.lock = threading.Lock()
 
 
@@ -16,8 +31,8 @@ class Config:
         section = 'LocalFiles'
         with self.lock:
             return {
-                'device_name': self.config.get(section, 'devicename', fallback='DefaultDevice'),
-                'local_directory': self.config.get(section, 'localdirectory', fallback='/default/path'),
+                'device_name': self.config.get(section, 'devicename'),
+                'local_directory': self.config.get(section, 'localdirectory'),
                 'check_interval': self.config.getint(section, 'checkinterval', fallback=30)
             }
 
@@ -25,10 +40,11 @@ class Config:
         section = 'BaiduCloud'
         with self.lock:
             return {
-                'app_name': self.config.get(section, 'appname', fallback=''),
-                'app_id': self.config.get(section, 'appid', fallback=''),
-                'app_key': self.config.get(section, 'appkey', fallback=''),
-                'secret_key': self.config.get(section, 'secretkey', fallback=''),
+                'app_name': self.config.get(section, 'appname', fallback='摄影素材自动备份'),
+                'app_id': self.config.get(section, 'appid', fallback='47097507'),
+                'app_key': self.config.get(section, 'appkey', fallback='H794OU88Q5KXH89ahoPGVCFNMxVBb1Sb'),
+                'secret_key': self.config.get(section, 'secretkey', fallback='pWjzs8MIBw2fxutAXsxVpN0Pxa0OqRT6'),
+                'sign_key' : self.config.get(section, 'signkey', fallback='X3JHR8D=5g0!EP%RF1FzGDrMQFPQkn1V'),
                 'access_token': self.config.get(section, 'accesstoken', fallback=''),
                 'refresh_token': self.config.get(section, 'refreshtoken', fallback=''),
             }
@@ -39,7 +55,7 @@ class Config:
 
         Args:
             section (str) : 需要更新的块区
-            updates (dic) : 等待更新的字典，注意：字典的 key 要用配置文件的大写模式如：AccessKey
+            updates (dic) : 等待更新的字典，注意：字典的 key 要用配置文件小写模式如：accesskey
         '''
         with self.lock:
             # 检查区域是否存在，如果不存在，则创建
@@ -57,11 +73,28 @@ class Config:
             
             mainlog.info(f"已更新配置文件 {section} 部分")
 
-
     def reload(self):
         with self.lock:
             self.config.read(self.filename)
 
+    def _check_config_required(self):
+        '''检查配置文件'''
+        # 定义必需的配置项
+        required_configs = {
+            'LocalFiles': ['devicename', 'localdirectory'],
+            'BaiduCloud': ['appname', 'appid', 'appkey', 'secretkey']
+        }
+
+        # 遍历必需的配置项进行检查
+        for section, keys in required_configs.items():
+            for key in keys:
+                values = self.config.get(section, key, fallback='')
+                if values=='':
+                    # 如果发现配置项为空，可以选择抛出异常或返回错误信息
+                    raise ValueError(f"配置项 {section}.{key} 是必需的，但目前为空。")
+
+        # 所有必需的配置项都存在且非空
+        return True
 
 # 创建一个 Config 实例
 # config = Config()
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,78 +1,287 @@
-import os
-import tempfile
-import unittest
-from unittest.mock import patch
-from config import Config  # 请替换为你的模块名
+import sys, os
+project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+src_path = os.path.join(project_path, 'src')
+sys.path.append(src_path)
 
-class TestConfig(unittest.TestCase):
+from configer import Config
+import pytest
 
-    def setUp(self):
-        # 创建临时文件
-        self.temp_file = tempfile.NamedTemporaryFile(delete=False)
-        config_content = """
-        [LocalFiles]
-        DeviceName=TestDevice
-        LocalDirectory=/test/path
-        CheckInterval=5
+@pytest.fixture
+def temp_config_file_of_normal_options(tmp_path):
+    '''正常的配置文件'''
+    temp_file = tmp_path / "temp_config.ini"
+    with open(temp_file, "w") as file:
+        file.write('''
+            [LocalFiles]
+            devicename = testdevice
+            localdirectory = /test/path
+            checkinterval = 30
 
-        [BaiduCloud]
-        AppID=test_app_id
-        AppKey=test_app_key
-        SecretKey=test_secret_key
-        AccessToken=test_access_token
-        RefreshToken=test_refresh_token
-        """
-        self.temp_file.write(config_content.encode())  # 将字符串编码为字节
-        self.temp_file.close()
+            [BaiduCloud]
+            appname = 摄影素材自动备份
+            appid = 47097507
+            appkey = H794OU88Q5KXH89ahoPGVCFNMxVBb1Sb
+            secretkey = pWjzs8MIBw2fxutAXsxVpN0Pxa0OqRT6
+            signkey = X3JHR8D=5g0!EP%RF1FzGDrMQFPQkn1V
+            accesstoken = 126.d4e31b6a2fb2281af4aa25ad6ee50999.YneUfftF6NPRxSiZZWRbLGhizfGyGV2S4lMuDyn.FeoBYA
+            refreshtoken = 127.52f65085968dc5d433d3bcab00b86e87.Ym6GUUjm-GWFnY6-QbH0JeXeVUPwN0ov-UqgX88.JTRDBQ
+            ''')
+    yield temp_file  
 
-        self.config = Config(self.temp_file.name)
+@pytest.fixture
+def temp_config_file_of_missing_required_options(tmp_path):
+    '''缺少必要参数的配置文件'''
+    temp_file = tmp_path / "temp_config.ini"
+    with open(temp_file, "w") as file:
+        file.write('''
+            [LocalFiles]
+            devicename = 
+            localdirectory = /test/path
+            checkinterval = 30
+
+            [BaiduCloud]
+            appname = 摄影素材自动备份
+            appid = 47097507
+            appkey = H794OU88Q5KXH89ahoPGVCFNMxVBb1Sb
+            secretkey = pWjzs8MIBw2fxutAXsxVpN0Pxa0OqRT6
+            signkey = X3JHR8D=5g0!EP%RF1FzGDrMQFPQkn1V
+            accesstoken = 126.d4e31b6a2fb2281af4aa25ad6ee50999.YneUfftF6NPRxSiZZWRbLGhizfGyGV2S4lMuDyn.FeoBYA
+            refreshtoken = 127.52f65085968dc5d433d3bcab00b86e87.Ym6GUUjm-GWFnY6-QbH0JeXeVUPwN0ov-UqgX88.JTRDBQ
+            ''')
+    yield temp_file  # 这将是你的测试配置文件
+    # 测试完成后，文件会自动删除，无需手动清理
+
+@pytest.fixture
+def temp_config_file_of_missing_optional_options(tmp_path):
+    '''缺少可选参数的配置文件'''
+    temp_file = tmp_path / "temp_config.ini"
+    with open(temp_file, "w") as file:
+        file.write('''
+            [LocalFiles]
+            devicename = testdevice
+            localdirectory = /test/path
+            checkinterval = 30
+
+            [BaiduCloud]
+            appname = 摄影素材自动备份
+            appid = 47097507
+            appkey = H794OU88Q5KXH89ahoPGVCFNMxVBb1Sb
+            secretkey = pWjzs8MIBw2fxutAXsxVpN0Pxa0OqRT6
+            signkey = X3JHR8D=5g0!EP%RF1FzGDrMQFPQkn1V
+            accesstoken = 
+            refreshtoken = 
+            ''')
+    yield temp_file  # 这将是你的测试配置文件
+    # 测试完成后，文件会自动删除，无需手动清理
+
+@pytest.fixture
+def temp_config_file_of_more_useless_options(tmp_path):
+    '''多了无用参数的配置文件'''
+    temp_file = tmp_path / "temp_config.ini"
+    with open(temp_file, "w") as file:
+        file.write('''
+            [LocalFiles]
+            devicename = testdevice
+            localdirectory = /test/path
+            checkinterval = 30
+            more3 = c
+                   
+            [BaiduCloud]
+            appname = 摄影素材自动备份
+            appid = 47097507
+            appkey = H794OU88Q5KXH89ahoPGVCFNMxVBb1Sb
+            secretkey = pWjzs8MIBw2fxutAXsxVpN0Pxa0OqRT6
+            signkey = X3JHR8D=5g0!EP%RF1FzGDrMQFPQkn1V
+            accesstoken = 126.d4e31b6a2fb2281af4aa25ad6ee50999.YneUfftF6NPRxSiZZWRbLGhizfGyGV2S4lMuDyn.FeoBYA
+            refreshtoken = 127.52f65085968dc5d433d3bcab00b86e87.Ym6GUUjm-GWFnY6-QbH0JeXeVUPwN0ov-UqgX88.JTRDBQ
+                   
+            more2 = b
+                   
+            [MoreConfig]
+            moreone = a
+            ''')
+    yield temp_file  # 这将是你的测试配置文件
+    # 测试完成后，文件会自动删除，无需手动清理
+
+@pytest.fixture
+def temp_config_file_of_more_useful_options(tmp_path):
+    '''多了有用参数的配置文件'''
+    temp_file = tmp_path / "temp_config.ini"
+    with open(temp_file, "w") as file:
+        file.write('''
+            [LocalFiles]
+            devicename = testdevice
+            localdirectory = /test/path
+            checkinterval = 30
+
+            [BaiduCloud]
+            appname = 摄影素材自动备份
+            appid = 47097507
+            appkey = H794OU88Q5KXH89ahoPGVCFNMxVBb1Sb
+            secretkey = pWjzs8MIBw2fxutAXsxVpN0Pxa0OqRT6
+            signkey = X3JHR8D=5g0!EP%RF1FzGDrMQFPQkn1V
+            accesstoken = token1
+            accesstoken = token2
+            refreshtoken = 127.52f65085968dc5d433d3bcab00b86e87.Ym6GUUjm-GWFnY6-QbH0JeXeVUPwN0ov-UqgX88.JTRDBQ
+            ''')
+    yield temp_file  # 这将是你的测试配置文件
+    # 测试完成后，文件会自动删除，无需手动清理
+
+@pytest.fixture
+def temp_config_file_of_mess_up_options(tmp_path):
+    '''正常的配置文件'''
+    temp_file = tmp_path / "temp_config.ini"
+    with open(temp_file, "w") as file:
+        file.write('''
+            [LocalFiles]
+            devicename = testdevice
+            localdirectory = /test/path
+            appname = 摄影素材自动备份
+            appid = 47097507
+
+            [BaiduCloud]
+            checkinterval = 30
+            appkey = H794OU88Q5KXH89ahoPGVCFNMxVBb1Sb
+            secretkey = pWjzs8MIBw2fxutAXsxVpN0Pxa0OqRT6
+            signkey = X3JHR8D=5g0!EP%RF1FzGDrMQFPQkn1V
+            accesstoken = 126.d4e31b6a2fb2281af4aa25ad6ee50999.YneUfftF6NPRxSiZZWRbLGhizfGyGV2S4lMuDyn.FeoBYA
+            refreshtoken = 127.52f65085968dc5d433d3bcab00b86e87.Ym6GUUjm-GWFnY6-QbH0JeXeVUPwN0ov-UqgX88.JTRDBQ
+            ''')
+    yield temp_file  
 
 
-    def tearDown(self):
-        # 测试完成后删除临时文件
-        os.remove(self.temp_file.name)
+def test_load_config_with_normal_options(temp_config_file_of_normal_options):
+    try:
+        # 初始化 Config 对象
+        config = Config(filename=str(temp_config_file_of_normal_options))
+    except Exception as e:
+        # 如果有任何异常抛出，则测试失败
+        pytest.fail(f"初始化 Config 时抛出异常: {e}")
 
+def test_load_config_with_missing_optional_options(temp_config_file_of_missing_optional_options):
+    try:
+        config = Config(filename=str(temp_config_file_of_missing_optional_options))
+    except Exception as e:
+        pytest.fail(f'缺少可选参数时初始化异常: {e}')
 
-    def test_get_local_config(self):
-        # 测试获取本地配置
-        local_config = self.config.get_local_config()
-        self.assertIn('device_name', local_config)
-        # 添加更多断言来验证值
+def test_load_config_with_missing_required_options(temp_config_file_of_missing_required_options):
+    try:
+        config = Config(str(temp_config_file_of_missing_required_options))
+        pytest.fail(f'缺少必要参数时初始化竟然没有报错')
 
+    except ValueError as e:
+        if '是必需的' in str(e):
+            assert True
+        else:
+            pytest.fail(f'缺少必要参数时初始化未能提出ValueError错误，而是{e}')
 
-    def test_get_baidu_config(self):
-        # 测试获取百度云配置
-        baidu_config = self.config.get_baidu_config()
-        self.assertIn('app_id', baidu_config)
-        # 添加更多断言来验证值
+def test_load_config_with_more_useless_options(temp_config_file_of_more_useless_options):
+    try:
+        # 初始化 Config 对象
+        config = Config(filename=str(temp_config_file_of_more_useless_options))
+    except Exception as e:
+        # 如果有任何异常抛出，则测试失败
+        pytest.fail(f"配置文件有额外无关参数时初始化异常: {e}")
 
+def test_load_config_with_more_useful_options(temp_config_file_of_more_useful_options):
+    try:
+        config = Config(str(temp_config_file_of_more_useful_options))
+        pytest.fail("有重复配置时初始化异常")
+    except:
+        # 只要爆出异常就行
+        assert True
 
-    def test_update_save(self):
-        section = 'BaiduCloud'
-        updates = {
-            'AccessToken' : 'new_token',
-            'RefreshToken' : 'new_refresh_token'
+def test_load_config_with_mess_up_options(temp_config_file_of_mess_up_options):
+    try:
+        config = Config(str(temp_config_file_of_mess_up_options))
+        pytest.fail('打乱配置时初始化异常')
+    except (ValueError):
+        assert True
+
+def test_load_config_with_no_file():
+    '''没有配置文件时自动创建并抛出异常'''
+    filename = 'tests/no_exsit.ini'
+    try:
+        config = Config(filename)
+        pytest.fail('文件不存在时加载异常')
+    except FileNotFoundError:
+        if os.path.exists(filename):
+            os.remove(filename)
+            assert True
+        else:
+            pytest.fail(f'检测不到配置文件，生成默认配置时异常')
+
+def test_get_local_config_with_normal(temp_config_file_of_normal_options):
+    '''正常文件能读取'''
+    config = Config(filename=str(temp_config_file_of_normal_options))
+
+    # 调用 get_local_config 方法
+    local_config = config.get_local_config()
+
+    # 验证返回结果
+    assert local_config['device_name'] == 'testdevice'
+    assert local_config['local_directory'] == '/test/path'
+    assert local_config['check_interval'] == 30
+
+    # 调用 get_baidu_config 方法
+    baidu_config = config.get_baidu_config()
+
+    # 验证
+    assert baidu_config['app_name'] == '摄影素材自动备份'
+    assert baidu_config['app_id'] == '47097507'
+    assert baidu_config['app_key'] == 'H794OU88Q5KXH89ahoPGVCFNMxVBb1Sb'
+    assert baidu_config['secret_key'] == 'pWjzs8MIBw2fxutAXsxVpN0Pxa0OqRT6'
+    assert baidu_config['sign_key'] == 'X3JHR8D=5g0!EP%RF1FzGDrMQFPQkn1V'
+    assert baidu_config['access_token'] == '126.d4e31b6a2fb2281af4aa25ad6ee50999.YneUfftF6NPRxSiZZWRbLGhizfGyGV2S4lMuDyn.FeoBYA'
+    assert baidu_config['refresh_token'] == '127.52f65085968dc5d433d3bcab00b86e87.Ym6GUUjm-GWFnY6-QbH0JeXeVUPwN0ov-UqgX88.JTRDBQ'
+
+def test_update_and_save_normal_options(temp_config_file_of_normal_options):
+    config = Config(temp_config_file_of_normal_options)
+    update_options = {
+        'accesstoken' : 'update_token',
+        'refreshtoken' : 'update_refresh'
+    }
+    section = 'BaiduCloud'
+    config.update_save(section, update_options)
+    config2 = Config(temp_config_file_of_normal_options)
+
+    localconfig1 = config.get_local_config()
+    localconfig2 = config2.get_local_config()
+
+    baiduconfig1 = config.get_baidu_config()
+    baiduconfig2 = config2.get_baidu_config()
+
+    assert localconfig1==localconfig2
+    assert baiduconfig1['app_name']==baiduconfig2['app_name']
+    assert baiduconfig1['app_id']==baiduconfig2['app_id']
+    assert baiduconfig1['app_key']==baiduconfig2['app_key']
+    assert baiduconfig1['secret_key']==baiduconfig2['secret_key']
+
+    assert baiduconfig2['access_token']=='update_token'
+    assert baiduconfig2['refresh_token']=='update_refresh'
+
+def test_update_and_save_no_exist_options():
+    try:
+        config = Config(temp_config_file_of_normal_options)
+        update_options = {
+            'accesstoken' : 'update_token',
+            'noexist' : 'what?'
         }
+        section = 'BaiduCloud'
+        config.update_save(section, update_options)
+        pytest.fail('更新不存在配置时未能提出异常')
+    except:
+        assert True
 
-        # 测试目标函数
-        self.config.update_save(section, updates)
-
-        # 验证
-        # 当前config实例内存已经成功更新
-        ac_now = self.config.get_baidu_config().get('access_token')
-        re_now = self.config.get_baidu_config().get('refresh_token')
-        self.assertEqual(ac_now, 'new_token')
-        self.assertEqual(re_now, 'new_refresh_token')
-
-        # 重新加载文件中的值，也能正确更新。
-        self.config.reload()
-        ac_reload = self.config.get_baidu_config().get('access_token')
-        re_reload = self.config.get_baidu_config().get('refresh_token')
-        self.assertEqual(ac_reload, 'new_token')
-        self.assertEqual(re_reload, 'new_refresh_token')
-
-
-    # 其他测试方法...
-
-if __name__ == '__main__':
-    unittest.main()
+def test_update_and_save_wrong_sections_options():
+    try:
+        config = Config(temp_config_file_of_normal_options)
+        update_options = {
+            'accesstoken' : 'update_token',
+            'refreshtoken' : 'update_refresh'
+        }
+        section = 'LocalFiles'
+        config.update_save(section, update_options)
+        pytest.fail('更新区块错误配置时未能提出异常')
+    except:
+        assert True
